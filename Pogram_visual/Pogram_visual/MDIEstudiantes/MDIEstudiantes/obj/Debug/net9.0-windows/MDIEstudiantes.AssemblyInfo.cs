d12643c14b140bbbd05e2d6f28b6e667bb714f2c
SteveenR-A <202854165+SteveenR-A@users.noreply.github.com>
2025-09-23 22:19:44 -0600

    Refactor code structure for improved readability and maintainability

--- Pogram_visual/Pogram_visual/MDIEstudiantes/MDIEstudiantes/obj/Debug/net9.0-windows/MDIEstudiantes.AssemblyInfo.cs
+++ Pogram_visual/Pogram_visual/MDIEstudiantes/MDIEstudiantes/obj/Debug/net9.0-windows/MDIEstudiantes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDIEstudiantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7a464b431dca728ab6f1c7e1f40c7d884da765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fa273f846e2ed93d9128d3053e1676d48e08c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDIEstudiantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDIEstudiantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
