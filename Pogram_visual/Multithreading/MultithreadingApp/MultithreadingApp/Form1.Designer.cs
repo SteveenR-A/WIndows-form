7f4b2b28fc7ada7e251bec922e72e5f7f869c538
SteveenR-A <202854165+SteveenR-A@users.noreply.github.com>
2025-10-10 20:52:15 +0000

    Añadir estructura básica del proyecto Multithreading con formularios y configuración inicial

---
+++ Pogram_visual/Multithreading/MultithreadingApp/MultithreadingApp/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace MultithreadingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.progressBurbuja = new System.Windows.Forms.ProgressBar();
+            this.progressQuickSort = new System.Windows.Forms.ProgressBar();
+            this.lblBurbuja = new System.Windows.Forms.Label();
+            this.lblQuickSort = new System.Windows.Forms.Label();
+            this.backgroundWorkerQuickSort = new System.ComponentModel.BackgroundWorker();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.LightBlue;
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGenerar.Location = new System.Drawing.Point(50, 80);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(150, 40);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar Datos";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnIniciar.Location = new System.Drawing.Point(220, 80);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(200, 40);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar Ordenamiento";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // progressBurbuja
+            // 
+            this.progressBurbuja.Location = new System.Drawing.Point(50, 180);
+            this.progressBurbuja.Name = "progressBurbuja";
+            this.progressBurbuja.Size = new System.Drawing.Size(350, 25);
+            this.progressBurbuja.TabIndex = 2;
+            // 
+            // progressQuickSort
+            // 
+            this.progressQuickSort.Location = new System.Drawing.Point(50, 250);
+            this.progressQuickSort.Name = "progressQuickSort";
+            this.progressQuickSort.Size = new System.Drawing.Size(350, 25);
+            this.progressQuickSort.TabIndex = 3;
+            // 
+            // lblBurbuja
+            // 
+            this.lblBurbuja.AutoSize = true;
+            this.lblBurbuja.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblBurbuja.Location = new System.Drawing.Point(50, 160);
+            this.lblBurbuja.Name = "lblBurbuja";
+            this.lblBurbuja.Size = new System.Drawing.Size(77, 17);
+            this.lblBurbuja.TabIndex = 4;
+            this.lblBurbuja.Text = "Burbuja: 0%";
+            // 
+            // lblQuickSort
+            // 
+            this.lblQuickSort.AutoSize = true;
+            this.lblQuickSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblQuickSort.Location = new System.Drawing.Point(50, 230);
+            this.lblQuickSort.Name = "lblQuickSort";
+            this.lblQuickSort.Size = new System.Drawing.Size(92, 17);
+            this.lblQuickSort.TabIndex = 5;
+            this.lblQuickSort.Text = "QuickSort: 0%";
+            // 
+            // backgroundWorkerQuickSort
+            // 
+            this.backgroundWorkerQuickSort.WorkerReportsProgress = true;
+            this.backgroundWorkerQuickSort.WorkerSupportsCancellation = false;
+            this.backgroundWorkerQuickSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerQuickSort_DoWork);
+            this.backgroundWorkerQuickSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerQuickSort_ProgressChanged);
+            this.backgroundWorkerQuickSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerQuickSort_RunWorkerCompleted);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblTitulo.Location = new System.Drawing.Point(100, 30);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(320, 26);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Ordenamiento Multihilo - Demo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblQuickSort);
+            this.Controls.Add(this.lblBurbuja);
+            this.Controls.Add(this.progressQuickSort);
+            this.Controls.Add(this.progressBurbuja);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multithreading - Ordenamiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ProgressBar progressBurbuja;
+        private System.Windows.Forms.ProgressBar progressQuickSort;
+        private System.Windows.Forms.Label lblBurbuja;
+        private System.Windows.Forms.Label lblQuickSort;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerQuickSort;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
